2ddc808b00ad4b3d64b46d46836765077cb9f455
MichealLiu6 <liuhonghouv@163.com>
2018-03-15 20:16:34 +0800

    add for j in range(i+1,len(nums))
    j>i

--- twoSum.py
+++ twoSum.py
@@ -2,10 +2,12 @@ def twoSum(nums,target):
 	i = 0;j = 0
 	while i<len(nums):
 		a = target - nums[i]
-		for j in range(len(nums)) :
+		for j in range(i+1,len(nums)) :
 			if a==nums[j]:
 				print([i,j])
 		i+=1
+
+
 
 
 
